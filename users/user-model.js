cceac3565680ecce686d5ed610210c6669e8ae53
Victoria  Topham <topham.victoria@gmail.com>
2020-05-18 14:21:53 -0700

    can successfully create a user with a hashed password

--- users/user-model.js
+++ users/user-model.js
@@ -2,9 +2,15 @@ const db = require('../db-config.js')
 
 module.exports = {
     getUsers,
+    insertUser
 }
 
 function getUsers() {
     return db.select('*')
         .from('users')
+}
+
+function insertUser(credentials) {
+    return db('users')
+        .insert(credentials);
 }
--- users/user-router.js
+++ users/user-router.js
@@ -1,18 +1,32 @@
 // /api
 
 const express = require('express')
-
+const bcrypt = require('bcryptjs')
 const router = express.Router();
+router.use(express.json())
 
 const Users = require('./user-model')
 
 //Creates a user using the information sent inside the body of the request. Hashes the password before saving the user to the database.
-router.post('/register', (req, res) => {
-    res.status(200).json({message: "You got to register"})
+router.post('/register', validateCredentials, (req, res) => {
+    
+
+    const credentials = req.body;
+    const hash = bcrypt.hashSync(credentials.password, 12);
+    credentials.password = hash;
+
+    Users.insertUser(credentials)
+        .then(id => {
+            res.status(200).json({data: id})
+        })
+        .catch(err => {
+            res.status(500).json({message: "Error adding user to database", error: err});
+        })
+
 })
 
 //Use the credentials sent inside the body to authenticate the user. On successful login, create a new session for the user and send back a 'Logged in' message and a cookie that contains the user id. If login fails, respond with the correct status code and the message: 'You shall not pass!'
-router.post('/login', validateCredentials, (req, res) => {
+router.post('/login', (req, res) => {
     
 })
 
@@ -20,16 +34,22 @@ router.post('/login', validateCredentials, (req, res) => {
 router.get('/users', (req, res) => {
     Users.getUsers()
         .then(users => {
-            res.status(200).json({data: users})
+            res.status(200).json({data: users});
         })
         .catch(err => {
-            res.status(500).json({message: "Error retrieving users from database", error: err})
-        })
+            res.status(500).json({message: "Error retrieving users from database", error: err});
+        });
 })
 
 
 function validateCredentials(req, res, next) {
-    return next();
+    if(req.body && req.body.username && req.body.password) {
+        next();
+    } else {
+        res.status(400).json({message: "Please include a username and password in the body of your request"})
+    }
 }
+
+
 
 module.exports = router;
